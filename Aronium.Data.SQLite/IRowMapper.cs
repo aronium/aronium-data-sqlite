1f0c3ac5b4f28d59fd05564e8287b41aeaf446b6
Bosko Kukanjac <bosko@aronium.com>
2017-07-09 02:14:59 +0200

    Typo fixed

--- Aronium.Data.SQLite/IRowMapper.cs
+++ Aronium.Data.SQLite/IRowMapper.cs
@@ -2,10 +2,14 @@
 
 namespace Aronium.Data.SQLite
 {
+    /// <summary>
+    /// Serves as a contract for RowMapper implementations.
+    /// </summary>
+    /// <typeparam name="T">Entity type to return.</typeparam>
     public interface IRowMapper<T>
     {
         /// <summary>
-        /// Maps data record to entity type.s
+        /// Maps data record to entity type.
         /// </summary>
         /// <param name="record">Data record.</param>
         /// <returns>Instance of {T}.</returns>
